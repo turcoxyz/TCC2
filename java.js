996ab7d4b5ca614d6172404b2fd9f26be6e33ab6
Cauã <caua.ribeirow@gmail.com>
2024-04-05 06:42:45 -0300

    andamento

---
+++ java.js
@@ -0,0 +1,14 @@
+var teste = document.getElementById("teste");
+var testeacao = document.getElementById("acao");
+
+testeacao.addEventListener("click", function () {
+
+    alert("NÃO ENCONTRADO");
+
+    /*if (teste.value.length > 0) {
+
+        alert(teste.value);
+    } else {
+        alert("sé é burro vei")
+    }*/
+});
